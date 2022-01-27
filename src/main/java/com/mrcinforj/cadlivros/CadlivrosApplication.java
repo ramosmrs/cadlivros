404e729c2db57fd07e3903d7c5a2f9767ce3ad83
ramosmrs <marcio.rsouza@gmail.com>
2022-01-27 19:37:55 -0300

    criação do projeto

---
+++ src/main/java/com/mrcinforj/cadlivros/CadlivrosApplication.java
@@ -0,0 +1,13 @@
+package com.mrcinforj.cadlivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadlivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadlivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrcinforj/cadlivros/CadlivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrcinforj.cadlivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadlivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
